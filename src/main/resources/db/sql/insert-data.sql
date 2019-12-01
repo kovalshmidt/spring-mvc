b1b439fde428b8b28b66388aad19b6805e5070d5
vitaliy <vkovalevskiy@omilia.com>
2019-12-01 21:29:21 +0200

    Deleted deprecated requests from springmvc. Updated ids in insert-data and add a new company name.

--- src/main/resources/db/sql/insert-data.sql
+++ src/main/resources/db/sql/insert-data.sql
@@ -1,7 +1,8 @@
-INSERT INTO users (id, fullName) VALUES (100, 'Stepan Giga');
-INSERT INTO users (id, fullName) VALUES (101, 'Viktor Pavlik');
+INSERT INTO users (id, fullName) VALUES (1, 'Stepan Giga');
+INSERT INTO users (id, fullName) VALUES (2, 'Viktor Pavlik');
 
 INSERT INTO phoneCompany (id, companyName) VALUES (22, 'beeline');
+INSERT INTO phoneCompany (id, companyName) VALUES (23, 'vodafone');
 
-INSERT INTO phoneNumber (id, phoneNumber, phoneCompany_id, users_id) VALUES (27, '380534265345', 22, 100);
-INSERT INTO phoneNumber (id, phoneNumber, phoneCompany_id, users_id) VALUES (28, '380534265345', 22, 101);
+INSERT INTO phoneNumber (id, phoneNumber, phoneCompany_id, users_id) VALUES (27, '380534265345', 22, 1);
+INSERT INTO phoneNumber (id, phoneNumber, phoneCompany_id, users_id) VALUES (28, '380534265345', 22, 2);
