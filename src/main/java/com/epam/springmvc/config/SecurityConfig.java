3f09a90e85ca1d64bc07d6d973f92bfba6b7832e
vitaliy <vkovalevskiy@omilia.com>
2019-12-07 15:50:58 +0200

    Created SecurityConfig and SecurityInitializer

---
+++ src/main/java/com/epam/springmvc/config/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.epam.springmvc.config;
+
+import com.epam.springmvc.security.CustomAuthenticationSuccessHandler;
+import com.epam.springmvc.service.UserDetailsServiceImpl;
+import com.epam.springmvc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan("com.epam.springmvc.security")
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/sign_up", "/login").anonymous()
+                .antMatchers("/user").authenticated()
+                .antMatchers("/users", "/uploadPage").hasRole("BOOKING_MANAGER")
+                .and().csrf().disable()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login/process")
+                .successHandler(getAuthenticationSuccessHandler())
+                .usernameParameter("email")
+                .failureUrl("/login?error=true")
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/users")
+                .and().logout();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(getUserDetailService());
+        auth.authenticationProvider(provider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler getAuthenticationSuccessHandler() {
+        return new CustomAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public UserDetailsService getUserDetailService() {
+        return new UserDetailsServiceImpl(userService);
+    }
+}
