e362407b55209897f6d70e9644f2f31235e23f91
vitaliy <vkovalevskiy@omilia.com>
2019-12-07 15:57:34 +0200

    Updated MainController

--- src/main/java/com/epam/springmvc/controller/MainController.java
+++ src/main/java/com/epam/springmvc/controller/MainController.java
@@ -1,10 +1,10 @@
 package com.epam.springmvc.controller;
 
 
+import com.epam.springmvc.model.PhoneUser;
 import com.epam.springmvc.model.PhoneUserInfo;
-import com.epam.springmvc.model.User;
 import com.epam.springmvc.service.PhoneUserInfoService;
-import com.epam.springmvc.service.UserService;
+import com.epam.springmvc.service.PhoneUserService;
 import com.epam.springmvc.utility.PdfUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -28,27 +28,33 @@ import static com.epam.springmvc.utility.PdfUtility.FILE_NAME;
 @Controller
 public class MainController {
 
-    private UserService userService;
+    private PhoneUserService phoneUserService;
     private PdfUtility pdfUtility;
     private PhoneUserInfoService phoneUserInfoService;
 
     @Autowired
-    public MainController(UserService userService, PdfUtility pdfUtility, PhoneUserInfoService phoneUserInfoService) {
-        this.userService = userService;
+    public MainController(PhoneUserService phoneUserService, PdfUtility pdfUtility, PhoneUserInfoService phoneUserInfoService) {
+        this.phoneUserService = phoneUserService;
         this.pdfUtility = pdfUtility;
         this.phoneUserInfoService = phoneUserInfoService;
     }
 
+    /**
+     * Main home page
+     */
     @GetMapping(value = {"/", "/index"})
     public String index(Model model) {
-        return "redirect:users";
+        return "homePage";
     }
 
+    /**
+     * Phone directory page with all phone users, accessible[BOOKING_MANAGER]
+     */
     @GetMapping(value = "/users")
-    public String getAllUsers(Model model) {
+    public String getAllPhoneUsers(Model model) {
 
         List<PhoneUserInfo> phoneUserInfos = new ArrayList<>();
-        List<Integer> usersIds = userService.findAll().stream().map(User::getId).collect(Collectors.toList());
+        List<Integer> usersIds = phoneUserService.findAll().stream().map(PhoneUser::getId).collect(Collectors.toList());
         for (Integer id : usersIds) {
             phoneUserInfos.add(phoneUserInfoService.createByUserId(id));
         }
@@ -57,14 +63,19 @@ public class MainController {
         return "usersList";
     }
 
+    /**
+     * User page with user info, accessible [REGISTERED_USER, BOOKING_MANAGER]
+     */
     @GetMapping(value = "/user/{id}")
     public String getById(@PathVariable("id") int id, Model model) {
 
         model.addAttribute("phoneUserInfo", phoneUserInfoService.createByUserId(id));
-        return "showUser";
+        return "showPhoneUser";
     }
 
-
+    /**
+     * The url to download all the phone users in pdf, accessible[BOOKING_MANAGER]
+     */
     @GetMapping(value = "/getUsersPdf")
     public ResponseEntity<byte[]> getUsersPDF() throws IOException {
 
@@ -81,6 +92,9 @@ public class MainController {
         return new ResponseEntity<>(contents, headers, HttpStatus.OK);
     }
 
+    /**
+     * The page on which is possible to upload a file with phone users, accessible[BOOKING_MANAGER]
+     */
     @GetMapping(value = "/uploadPage")
     public String toUploadPage() {
         return "uploadPage";
