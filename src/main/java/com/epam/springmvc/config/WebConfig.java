83d5ae4eb0d9df8abac68ebb837d6cf98945253c
vitaliy <vkovalevskiy@omilia.com>
2019-12-10 17:05:28 +0200

    created resources folder in webapp with css and js files. Added Bootstrap. Configured WebConfig to use the resources from resources folder.

--- src/main/java/com/epam/springmvc/config/WebConfig.java
+++ src/main/java/com/epam/springmvc/config/WebConfig.java
@@ -6,13 +6,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.epam.springmvc.controller")
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+    }
 
     @Bean(name = "viewResolver")
     public ViewResolver getViewResolver() {
@@ -27,7 +36,7 @@ public class WebConfig {
     @Bean
     public FreeMarkerConfigurer freemarkerConfig() {
         FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
-        freeMarkerConfigurer.setTemplateLoaderPath("/WEB-INF/views");
+        freeMarkerConfigurer.setTemplateLoaderPaths("/", "/WEB-INF/views/", "/WEB-INF/views/exceptions", "/WEB-INF/views/auth");
         return freeMarkerConfigurer;
     }
 
