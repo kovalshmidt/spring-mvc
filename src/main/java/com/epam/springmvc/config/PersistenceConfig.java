56e44e905259f8c019d96014876a51e1f2055abe
vitaliy <vkovalevskiy@omilia.com>
2019-12-12 16:28:34 +0200

    Added PlatformTransactionManager to work with transactions.

--- src/main/java/com/epam/springmvc/config/PersistenceConfig.java
+++ src/main/java/com/epam/springmvc/config/PersistenceConfig.java
@@ -4,20 +4,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("com.epam.springmvc.dao")
+@EnableTransactionManagement
 public class PersistenceConfig {
+
+    /**
+     * Set the datasource in use here, so that all the beans use the same
+     */
+    private DataSource getDatasource() {
+        return getDataSourceH2();
+    }
 
     @Bean
     public JdbcTemplate getJdbcTemplate() {
         //Choose the Datasource between MySQL and H2
-        return new JdbcTemplate(getDataSourceMysql());
+        return new JdbcTemplate(getDatasource());
     }
 
     /**
@@ -37,7 +48,7 @@ public class PersistenceConfig {
     }
 
     /**
-     * For Msql Database
+     * For Mysql Database
      */
     @Bean
     public DataSource getDataSourceMysql() {
@@ -47,5 +58,10 @@ public class PersistenceConfig {
         dataSource.setPassword("nettuno27");
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager txManager(){
+        return new DataSourceTransactionManager(getDatasource());
     }
 }
