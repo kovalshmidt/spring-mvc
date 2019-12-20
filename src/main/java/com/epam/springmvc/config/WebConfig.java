7bdef7f822739a1dcf7dbb266378280def0967ae
vitaliy <vkovalevskiy@omilia.com>
2019-12-20 17:19:12 +0200

    Configured message converters and view resolvers. Added Jackson in pom

--- src/main/java/com/epam/springmvc/config/WebConfig.java
+++ src/main/java/com/epam/springmvc/config/WebConfig.java
@@ -1,20 +1,44 @@
 package com.epam.springmvc.config;
 
+import com.epam.springmvc.converter.PdfHttpMessageConverter;
+import com.epam.springmvc.service.UserClientService;
+import com.epam.springmvc.service.UserClientServiceImpl;
+import com.epam.springmvc.utility.PdfUtility;
+import com.epam.springmvc.viewResolvers.PdfViewResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.epam.springmvc.controller")
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    PdfUtility pdfUtility;
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.ignoreAcceptHeader(false);
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -23,14 +47,50 @@ public class WebConfig implements WebMvcConfigurer {
                 .addResourceLocations("/resources/");
     }
 
-    @Bean(name = "viewResolver")
-    public ViewResolver getViewResolver() {
-        FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
-        viewResolver.setOrder(1);
-        viewResolver.setCache(true);
-        viewResolver.setPrefix("");
-        viewResolver.setSuffix(".ftl");
-        return viewResolver;
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
+        converters.add(new PdfHttpMessageConverter(pdfUtility));
+    }
+
+    @Bean
+    public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+        resolver.setContentNegotiationManager(manager);
+
+        // Define all possible view resolvers
+        List<ViewResolver> resolvers = new ArrayList<ViewResolver>();
+
+        resolvers.add(jsonViewResolver());
+        resolvers.add(pdfViewResolver());
+        resolvers.add(ftlViewResolver());
+
+        resolver.setViewResolvers(resolvers);
+        return resolver;
+    }
+
+    @Bean
+    public ViewResolver jsonViewResolver() {
+        return (viewName, locale) -> {
+            MappingJackson2JsonView jsonViewResolver = new MappingJackson2JsonView();
+            jsonViewResolver.setPrettyPrint(true);
+            return jsonViewResolver;
+        };
+    }
+
+    @Bean
+    public ViewResolver pdfViewResolver() {
+        return (viewName, locale) -> new PdfViewResolver(pdfUtility);
+    }
+
+    @Bean
+    public ViewResolver ftlViewResolver() {
+        FreeMarkerViewResolver ftlViewResolver = new FreeMarkerViewResolver();
+        ftlViewResolver.setOrder(1);
+        ftlViewResolver.setCache(true);
+        ftlViewResolver.setPrefix("");
+        ftlViewResolver.setSuffix(".ftl");
+        return ftlViewResolver;
     }
 
     @Bean
@@ -46,4 +106,15 @@ public class WebConfig implements WebMvcConfigurer {
         multipartResolver.setMaxUploadSize(100000);
         return multipartResolver;
     }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public UserClientService userClientService() {
+        return new UserClientServiceImpl(restTemplate());
+    }
+
 }
